f34cc13786ac4ab1a55b5ac676fbb227684fdad6
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-12-24 01:12:12 -0300

    chore: add tools for code pattern

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  plugins: ["prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "no-underscore-dangle": "off",
+    "import/prefer-default-export": "off",
+    "func-names": "off",
+    "consistent-return": "off",
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    camelcase: "off",
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+  },
+  settings: {
+    "import/resolver": {
+      alias: {
+        map: [
+          ["@src", "./src"],
+          ["@test", "./test"],
+        ],
+        extensions: [".ts", ".js", ".jsx", ".json"],
+      },
+    },
+  },
+};
