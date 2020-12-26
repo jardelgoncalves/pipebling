ce47466b1d14c885519ec4b7e39860fd35e0ebef
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-12-26 11:16:24 -0300

    feat: add deals model

---
+++ src/models/Deals.js
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from 'mongoose';
+
+const schema = new Schema(
+  {
+    deals_ids: {
+      type: [Number],
+    },
+    period: {
+      type: String,
+    },
+    totals_by_currency: {
+      type: String,
+      get(data) {
+        try {
+          return JSON.parse(data);
+        } catch (err) {
+          return data;
+        }
+      },
+      set(data) {
+        return JSON.stringify(data);
+      },
+    },
+    totals_converted: {
+      type: String,
+      get(data) {
+        try {
+          return JSON.parse(data);
+        } catch (err) {
+          return data;
+        }
+      },
+      set(data) {
+        return JSON.stringify(data);
+      },
+    },
+  },
+  {
+    toJSON: {
+      transform: (_, ret) => {
+        ret.id = ret._id;
+
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+export const Deals = mongoose.model('Deal', schema);
