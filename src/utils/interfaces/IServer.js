fd2199266a54530adb995d896f46b58157c50a80
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-12-26 14:12:47 -0300

    feat: add addRoutes method to apply routes of the application

--- src/utils/interfaces/IServer.js
+++ src/utils/interfaces/IServer.js
@@ -1,10 +1,28 @@
 import express from 'express';
 import { NotImplementedException } from '../NotImplementedException';
 
+const routerInstance = Symbol('routerInstance');
 export class IServer {
   constructor(app = express) {
     this.app = app();
     this.bodyParser = express.json;
+    this[routerInstance] = express.Router;
+  }
+
+  /**
+   * @param {Array.<{prefix: String, router: Function}>} mapRoutes - project route array.
+   * @param {import('@src/services').ServiceManager} serviceManager - Service Manager instance.
+   */
+  addRoutes(mapRoutes = [], serviceManager) {
+    const routes = this[routerInstance]();
+
+    mapRoutes.forEach((route) => {
+      routes.use(
+        route.prefix,
+        route.router(this[routerInstance], serviceManager)
+      );
+    });
+    this.app.use(routes);
   }
 
   async init() {
