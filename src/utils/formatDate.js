7a55099119d978e38e8cb8c929ef7c0436ff4d68
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2020-12-28 00:17:47 -0300

    fix: correcting env of timezone

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -2,7 +2,9 @@ import config from 'config';
 
 export const formatDate = (date = new Date(), BR = false) => {
   const data = date
-    .toLocaleString('pt-BR', { timezone: config.get('App.cron.timezone') })
+    .toLocaleString('pt-BR', {
+      timezone: config.get('App.resources.cron.timezone'),
+    })
     .split(',')[0];
 
   return BR ? data : data.split('/').reverse().join('-');
